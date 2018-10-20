bdd1510e8ba36e8adcd31760084c152f2d3478b4
7965 <7577965@gmail.com>
2018-10-20 13:16:59 +0300

    счетчики

---
+++ web_register/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebRegisterConfig(AppConfig):
+    name = 'web_register'
---
+++ web_register/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+class Old_reg(models.Model):
+    oldHot = models.IntegerField()
+    oldCold = models.IntegerField()
+    oldElec = models.IntegerField()
+
+    def pass
+
+class Price(models.Model):
+    prHot = models.FloatField()
+    prCold = models.FloatField()
+    prElec = models.FloatField()
+    prStock = models.FloatField()
+
+    def pass
+
+class Now_reg(models.Model):
+    nowHot = models.IntegerField()
+    nowCold = models.IntegerField()
+    nowElec = models.IntegerField()
+
+    def pass
+# Create your models here.
